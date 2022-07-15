14f8a8e863cfc891698dcaf06f1a8ab112fb2271
SriArvindR-JFT <sriarvind.r@jellyfishtechnologies.com>
2022-07-15 18:28:10 +0530

    Task 12 & Questions Updated

--- Task12.js
+++ Task12.js
@@ -1,2 +1,33 @@
 // Q12. Write a program to find the occurrences of each number of an array of 20 elements;
 
+const findOccurrences = (arr) => {
+
+    const occurrences = new Map();
+
+    let i = 0;
+
+    while (i < arr.length) {
+
+        let count = 0;
+
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                count++;
+            }
+        }
+
+        occurrences.set(arr[i], count);
+
+        arr = arr.filter((item) => {
+            return item !== arr[i];
+        });
+
+    }
+
+    return occurrences;
+
+};
+
+const arr = [1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6];
+
+console.log(findOccurrences(arr));
